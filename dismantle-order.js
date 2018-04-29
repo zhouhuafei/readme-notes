78774a4285a5c75bc9a743e946d7189c911863be
zhouhuafei <1123486116@qq.com>
2018-04-30 00:59:34 +0800

    md

--- dismantle-order.js
+++ dismantle-order.js
@@ -1,24 +1,67 @@
 // 拆订单，使用优惠金额把账做平
-const data = {
-    discountPrice: '198.85', // 主订单优惠金额
+const order = {
+    id: '0', // 主订单号
+    totalPrice: '199.86', // 主订单总价
+    freight: '0.02', // 主订单运费
     payPrice: '0.03', // 主订单实付金额
-    freight: null, // 主订单运费
-    totalPrice: null, // 主订单总价
-    item: [
+    coupon: '199.85', // 主订单优惠券
+    fullReduction: '0.00', // 主订单满减
+    goods: [ // 商品
         {
-            price: '99.92', // 子订单商品价格
-            freight: '0.02', // 子订单运费
-            payPrice: null, // 子订单实付金额
-            discountPrice: null, // 子订单优惠金额
+            supplier: { // 供应商
+                id: '0', // 供应商id
+            },
+            price: '99.92', // 价格
+            freight: '0.02', // 运费
         },
         {
-            price: '0.01', // 子订单商品价格
-            freight: '0.00', // // 子订单运费
-            payPrice: null, // 子订单实付金额
-            discountPrice: null, //子订单优惠金额
+            supplier: { // 供应商
+                id: '1', // 供应商id
+            },
+            price: '0.01', // 价格
+            freight: '0.00', // 运费
         },
     ],
 };
+
+// 期望结果
+const result = [
+    {
+        id: '0-0', // 子订单号
+        totalPrice: '199.86', // 子订单总价
+        freight: '0.02', // 子订单运费
+        payPrice: '0.03', // 子订单实付金额
+        coupon: '199.84', // 子订单优惠券
+        fullReduction: '0.00', // 子订单满减
+        goods: [
+            {
+                supplier: { // 供应商
+                    id: '0', // 供应商id
+                },
+                price: '99.92', // 价格
+                freight: '0.02', // 运费
+            },
+        ],
+    },
+    {
+        id: '0-1', // 子订单号
+        totalPrice: '0.01', // 子订单总价
+        freight: '0.00', // 子订单运费
+        payPrice: '0.00', // 子订单实付金额
+        coupon: '0.01', // 子订单优惠券
+        fullReduction: '0.00', // 子订单满减
+        goods: [
+            {
+                supplier: { // 供应商
+                    id: '1', // 供应商id
+                },
+                price: '0.01', // 价格
+                freight: '0.00', // 运费
+            },
+        ],
+    },
+];
+
 
 // 拆单业务逻辑
 function dismantleOrder(data) {
@@ -27,4 +70,4 @@ function dismantleOrder(data) {
     });
 }
 
-console.log(dismantleOrder());
+console.log(dismantleOrder(order));
