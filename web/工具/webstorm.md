a3e489b407b18a02ba9a65b20ba0d684fba78bcf
zhouhuafei <zhouhuafei@shopex.cn>
2018-09-21 14:21:56 +0800

    md

--- web/工具/webstorm.md
+++ web/工具/webstorm.md
@@ -8,3 +8,6 @@
 * 希望后续WebStorm官方可以提供微信小程序的语法支持。
 * 如果开发微信小程序的话，```HBuilderX编辑器```的语法提示做的就非常完善。
 * 微信开发者工具的提示最是完善，毕竟是本家的工具。
+
+# eslint
+* 如果没开启es6，使用const定义变量时，eslint检测工具会报错：```Const definitions are not supported by current JavaScript version```
--- web/算法/算法-冒泡排序.js
+++ web/算法/算法-冒泡排序.js
@@ -1,10 +1,10 @@
-var arr = [7, 8, 7, 9, 10, 2, 4, 3, 6, 7, 1, 5];
+const arr = [7, 8, 7, 9, 10, 2, 4, 3, 6, 7, 1, 5];
 
 function sort(arr) {
-    for (var i = 0; i < arr.length; i++) {
-        for (var j = i + 1; j < arr.length; j++) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
             if (arr[i] > arr[j]) {
-                var temp = arr[i];
+                const temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
@@ -13,4 +13,4 @@ function sort(arr) {
     return arr;
 }
 
-console.log(sort(arr));
+console.log('冒泡排序的结果：', sort(arr));
