f56301e7e12f822c91bafe4b767d1c954822be7b
zhouhuafei <zhouhuafei@shopex.cn>
2018-09-21 14:09:03 +0800

    冒泡排序

--- web/算法/算法-冒泡排序.js
+++ web/算法/算法-冒泡排序.js
@@ -1,6 +1,15 @@
 var arr = [7, 8, 7, 9, 10, 2, 4, 3, 6, 7, 1, 5];
 
 function sort(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                var temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
     return arr;
 }
 
