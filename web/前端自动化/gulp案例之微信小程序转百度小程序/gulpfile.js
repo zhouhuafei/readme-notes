94161a721ce932d977c0d68aba1e277d61b1ab68
zhouhuafei <1123486116@qq.com>
2018-11-07 20:54:37 +0800

    md

--- web/前端自动化/gulp案例之微信小程序转百度小程序/gulpfile.js
+++ web/前端自动化/gulp案例之微信小程序转百度小程序/gulpfile.js
@@ -1,14 +1,9 @@
-/*
-1. 关于.wxml后缀：我本想使用.html后缀进行开发，但是本项目要拷贝另外一个项目，另外一个项目是.wxml后缀，所以我就保留了.wxml后缀
-*/
 const gulp = require('gulp');
 const rename = require('gulp-rename');
 const plumber = require('gulp-plumber'); // 报错就中断？使用这个处理一下即可。
 const sass = require('gulp-sass');
 const imagemin = require('gulp-imagemin');
-const through = require('through2'); // 编写gulp插件
-// const replace = require('gulp-replace'); // 内容替换(这个替换的不完整，会漏掉某些文件。我使用此包时，有些文件并没有被转换成功)
-const replace = require('gulp-batch-replace'); // 内容替换(这个没问题，上个包出的问题，这个包没出类似的问题)
+const replace = require('gulp-batch-replace'); // 内容替换
 const envName = process.env.NODE_ENV; // 当前环境
 const isProduction = envName === 'production'; // 是否是生产环境
 
@@ -26,6 +21,7 @@ function fn(name) {
             .pipe(replace([
                 [isProduction ? 'domainPathBuild' : 'domainPathDev', 'domainPath'], // 生产环境和开发环境使用的domainPath不同
                 [isProduction ? 'h5DomainBuild' : 'h5DomainDev', 'h5Domain'], // 生产环境和开发环境使用的h5Domain不同
+                [isProduction ? 'envBuild' : 'envDev', 'env'], // 生产环境和开发环境使用
             ]))
             .pipe(gulp.dest(`dist/`));
         // 百度小程序使用ext.js，因目前不支持第三方
@@ -34,6 +30,7 @@ function fn(name) {
             .pipe(replace([
                 [isProduction ? 'domainPathBuild' : 'domainPathDev', 'domainPath'], // 生产环境和开发环境使用的domainPath不同
                 [isProduction ? 'h5DomainBuild' : 'h5DomainDev', 'h5Domain'], // 生产环境和开发环境使用的h5Domain不同
+                [isProduction ? 'envBuild' : 'envDev', 'env'], // 生产环境和开发环境使用
             ]))
             .pipe(gulp.dest(`dist/`));
     });
@@ -44,8 +41,10 @@ function fn(name) {
             // 这里先隐掉，后续百度平台支持第三方平台的时候可以放出来，不过后续出的今日头条小程序是否支持就另说了。如果后续这里放出来，diff里的ext.js和ext.json就可以删掉，然后放这里统一处理就行了。
             // .pipe(replace([
             //     ['"mp_platform": "weixin",', name === 'baidu' ? '"mp_platform": "baidu",' : '"mp_platform": "weixin",'], // ext.json中微信的某个字段换成百度的字段。
-            //     [isProduction ? 'domainPathBuild' : 'domainPathDev', 'domainPath'], // 生产环境和开发环境使用的domainPath不同
-            //     [isProduction ? 'h5DomainBuild' : 'h5DomainDev', 'h5Domain'], // 生产环境和开发环境使用的h5Domain不同
+            //     ['"api_type": "weixin",', name === 'baidu' ? '"api_type": "baidu",' : '"api_type": "weixin",'], // ext.json中微信的某个字段换成百度的字段。
+            //     [isProduction ? "domainPathBuild" : "domainPathDev", "domainPath"], // 生产环境和开发环境使用的domainPath不同
+            //     [isProduction ? "h5DomainBuild" : "h5DomainDev", "h5Domain"], // 生产环境和开发环境使用的h5Domain不同
+            //     [isProduction ? "envBuild" : "envDev", "env"], // 生产环境和开发环境使用
             // ]))
             .pipe(gulp.dest(`dist/${name}/`));
     });
@@ -55,8 +54,8 @@ function fn(name) {
             .pipe(plumber())
             .pipe(replace([
                 ['wx:', name === 'baidu' ? 's-' : 'wx:'], // 微信小程序的模板语法换成百度小程序的模板语法。
-                ['.wxml', name === 'baidu' ? '.swan' : '.wxml'], // 此项是针对富文本解析进行的替换。具体请查看detail.wxml。
-                ['{{wxParseData:detailInfo.nodes}}', name === 'baidu' ? '{{ {wxParseData:detailInfo.nodes} }}' : '{{wxParseData:detailInfo.nodes}}'], // 此项是针对富文本解析进行的替换。具体请查看detail.wxml。
+                ['.wxml', name === 'baidu' ? '.swan' : '.wxml'], // 此项是针对富文本解析进行的替换。
+                ['{{wxParseData:content.nodes}}', name === 'baidu' ? '{{ {wxParseData:content.nodes} }}' : '{{wxParseData:content.nodes}}'], // 此项是针对富文本解析进行的替换。
             ]))
             .pipe(rename((path) => {
                 path.extname = name === 'baidu' ? '.swan' : '.wxml'; // 百度的后缀和微信的后缀不同。
