b1a8bcfd246ab18a83a0f992b4eda0e4d56a9f85
zhouhuafei <1123486116@qq.com>
2021-03-26 17:05:54 +0800

    feat: 🎸 h5 vue 实现微信小程序scroll-view的部分功能

---
+++ web/踩坑小程序1-微信小程序/scroll-view.vue
@@ -0,0 +1,70 @@
+<template>
+  <!--h5 vue 实现微信小程序scroll-view的部分功能-->
+  <div
+    ref="wrap"
+    class="wrap"
+    :class="[scrollX?'scrollX':'', scrollY?'scrollY':'']"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'scroll-view',
+  props: {
+    'scroll-x': {
+      type: Boolean,
+      default: false
+    },
+    'scroll-y': {
+      type: Boolean,
+      default: false
+    }
+  },
+  mounted () {
+    this.wrap = this.$refs.wrap
+    this.$refs.wrap.addEventListener('scroll', this.scroll)
+  },
+  methods: {
+    scroll () {
+      const wrap = this.wrap
+      if (this.scrollX) {
+        if (wrap.scrollLeft === 0) {
+          this.$emit('scrolltoupper')
+        }
+        if (wrap.scrollLeft + wrap.offsetWidth === wrap.scrollWidth) {
+          this.$emit('scrolltolower')
+        }
+      }
+      if (this.scrollY) {
+        if (wrap.scrollTop === 0) {
+          this.$emit('scrolltoupper')
+        }
+        if (wrap.scrollTop + wrap.offsetHeight === wrap.scrollHeight) {
+          this.$emit('scrolltolower')
+        }
+      }
+      this.$emit('scroll')
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .wrap {
+    overflow: hidden;
+
+    &.scrollX {
+      overflow-x: auto;
+    }
+
+    &.scrollY {
+      overflow-y: auto;
+    }
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+</style>
