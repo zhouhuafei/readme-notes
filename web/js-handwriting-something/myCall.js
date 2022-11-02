af8de573e3362e003ce9b70d9ac5b2c033181eb5
zhouhuafei <1123486116@qq.com>
2022-11-02 12:49:31 +0800

    feat: 🎸 myCall

--- web/js-handwriting-something/myCall.js
+++ web/js-handwriting-something/myCall.js
@@ -1,1 +1,22 @@
-// ...TODO
+/* eslint-disable no-extend-native */
+
+Function.prototype.myCall = function (thisIsMe, ...args) {
+  if ([null, undefined].indexOf(thisIsMe) !== -1) {
+    thisIsMe = globalThis
+  } else {
+    thisIsMe = Object(thisIsMe)
+  }
+  const fn = Symbol('fn')
+  Object.defineProperty(thisIsMe, fn, { value: this })
+  const result = thisIsMe[fn](...args)
+  delete thisIsMe[fn]
+  return result
+}
+
+function fn (arg) {
+  console.log('------this', this)
+  console.log('------arg', arg)
+  console.log('------this.a', this.a)
+}
+
+fn.myCall({ a: 'a' }, 'arg')
