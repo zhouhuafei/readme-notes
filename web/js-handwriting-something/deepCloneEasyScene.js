d192d8b37c6b52488c4c571445d7489329bc8134
zhouhuafei <1123486116@qq.com>
2022-11-08 15:40:29 +0800

    style: 💄 js - 深拷贝 - 对象循环引用处理

--- web/js-handwriting-something/deepCloneEasyScene.js
+++ web/js-handwriting-something/deepCloneEasyScene.js
@@ -17,7 +17,6 @@ function deepCloneEasyScene (obj, wm = new WeakMap()) {
       const isObject = Object.prototype.toString.call(val).slice(8, -1) === 'Object'
       const isArray = Object.prototype.toString.call(val).slice(8, -1) === 'Array'
       if (isObject || isArray) {
-        wm.set(val, val)
         newObj[key] = deepCloneEasyScene(val, wm)
       } else {
         newObj[key] = val
@@ -34,6 +33,7 @@ const newObjFirst = deepCloneEasyScene(obj)
 console.log('深拷贝 - 普通对象------obj', obj)
 console.log('深拷贝 - 普通对象------newObjFirst', newObjFirst)
 console.log('深拷贝 - 普通对象------obj === newObjFirst', obj === newObjFirst)
+console.log('')
 
 // 深拷贝 - 循环引用对象
 const obj2 = { a: 1, b: 2, c: 3 }
@@ -43,17 +43,23 @@ console.log('深拷贝 - 循环引用对象------obj2', obj2)
 console.log('深拷贝 - 循环引用对象------newObj2', newObj2)
 console.log('深拷贝 - 循环引用对象------obj2 === newObj2', obj2 === newObj2)
 console.log('深拷贝 - 循环引用对象------obj2 === newObj2.obj2', obj2 === newObj2.obj2)
+console.log('深拷贝 - 循环引用对象------obj2 === newObj2.obj2.obj2', obj2 === newObj2.obj2.obj2)
 console.log('深拷贝 - 循环引用对象------obj2.obj2 === newObj2.obj2', obj2.obj2 === newObj2.obj2)
+console.log('深拷贝 - 循环引用对象------obj2.obj2 === newObj2.obj2.obj2', obj2.obj2 === newObj2.obj2.obj2)
+console.log('')
 
 // 深拷贝 - 暴力实现方式
 const newObjLast = JSON.parse(JSON.stringify(obj))
 console.log('深拷贝 - 暴力实现方式------newObjLast', newObjLast)
 console.log('深拷贝 - 暴力实现方式------obj === newObjLast', obj === newObjLast)
+console.log('')
 
 // 弊端：JSON序列化时会把：undefined/函数（包含构造函数）/Symbol对象 忽略掉。
 console.log('JSON序列化时会把：undefined 忽略', JSON.stringify({ a: undefined }))
 console.log('JSON序列化时会把：函数（包含构造函数） 忽略', JSON.stringify({ a: Symbol }))
 console.log('JSON序列化时会把：Symbol对象 忽略', JSON.stringify({ a: Symbol('1') }))
+console.log('')
+
 // 弊端：JSON序列化时会把：Map对象/WeakMap对象/Set对象/WeakSet对象/正则/Error对象 转为空json对象。
 console.log('JSON序列化时会把：Map对象 转换为空对象', JSON.stringify({ a: new Map([['k1', 'v1'], ['k2', 'v2']]) }))
 console.log('JSON序列化时会把：Set对象 转换为空对象', JSON.stringify({ a: new Set([1, 2, 3]) }))
