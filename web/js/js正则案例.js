cc9fc6b9f850ebfa465b8167b0412b9f37fc1140
zhouhuafei <1123486116@qq.com>
2024-12-12 13:54:11 +0800

    feat: 🎸 正则案例

--- web/js/js正则案例.js
+++ web/js/js正则案例.js
@@ -1,4 +1,4 @@
-// 案例1：把类似 16rpx 格式的字符串转换为 px2rpx(16) 格式的字符串
+// 案例：把类似 16rpx 格式的字符串转换为 px2rpx(16) 格式的字符串 - 转换后配合scss可以把小程序上的组件样式复制到管理后台使用
 {
   let str = `
   .view {
@@ -6,10 +6,11 @@
     width:  300rpx; height:    200rpx;
   }
   `
-  str = str.replace(/(\d*)rpx/ig, (match, firstGroup) => `px2rpx(${firstGroup})`)
+  str = str.replace(/(?<!px)(\d+)rpx/ig, (match, firstGroup) => `px2rpx(${firstGroup})`)
   console.log('str：', str)
 }
-// 案例2：把类似 14px 格式的字符串转换为 28px 格式的字符串
+
+// 案例：把类似 14px 格式的字符串转换为 28px 格式的字符串 - 可以用来解决微信小程序富文本渲染单位px无法自适应的问题
 {
   let str = `
   <p style="font-size:14px;"></p>       <p style="font-size: 16px;"></p>
