b4031a93e2897b978cb60aaa4e173700e8aa685b
zhouhuafei <1123486116@qq.com>
2025-10-20 20:09:38 +0800

    feat: 🎸 算法-找出数组中第三大的值

---
+++ web/算法/算法-找出数组中第三大的值.js
@@ -0,0 +1,20 @@
+const getThirdMax = (array) => {
+  let first = -Infinity, second = -Infinity, third = -Infinity
+  for (let i = 0; i < array.length; i++) {
+    const num = array[i]
+    if (num === first || num === second || num === third) continue
+    if (num > first) {
+      third = second
+      second = first
+      first = num
+    } else if (num > second) {
+      third = second
+      second = num
+    } else if (num > third) {
+      third = num
+    }
+  }
+  return third === -Infinity ? null : third
+}
+
+console.log(getThirdMax([2, 5, 3, 9, 1, 6]))
